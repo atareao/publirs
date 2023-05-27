c9d772c971a8396845901529872b2ad53e73054b
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-05-27 08:44:09 +0200

    tips

--- migrations/20230527051603_tips.up.sql
+++ migrations/20230527051603_tips.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tips (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     category_id INTEGER,
-    tip STRING,
+    text STRING,
     published BOOLEAN
 );
--- src/models/tip.rs
+++ src/models/tip.rs
@@ -2,33 +2,33 @@ use serde::{Serialize, Deserialize};
 use sqlx::{sqlite::{SqlitePool, SqliteRow}, query, Row};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Option{
+pub struct Tip{
     id: i64,
-    reto_id: i64,
-    option: String,
-    #[serde(default = "get_default_isok")]
-    isok: bool
+    category_id: i64,
+    text: String,
+    #[serde(default = "get_default_published")]
+    published: bool
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct NewTip{
-    reto_id: i64,
-    option: String,
-    #[serde(default = "get_default_isok")]
-    isok: bool
+    category_id: i64,
+    text: String,
+    #[serde(default = "get_default_published")]
+    published: bool
 }
 
-fn get_default_isok() -> bool{
+fn get_default_published() -> bool{
     false
 }
 
-impl Option{
+impl Tip{
     fn from_row(row: SqliteRow) -> Self{
         Self{
             id: row.get("id"),
-            reto_id: row.get("reto_id"),
-            option: row.get("option"),
-            isok: row.get("isok")
+            category_id: row.get("category_id"),
+            text: row.get("text"),
+            published: row.get("published")
         }
     }
 
@@ -36,33 +36,33 @@ impl Option{
         self.id
     }
 
-    pub fn get_reto_id(&self) -> i64{
-        self.reto_id
+    pub fn get_category_id(&self) -> i64{
+        self.category_id
     }
 
-    pub fn get_option(&self) -> &str{
-        &self.option
+    pub fn get_text(&self) -> &str{
+        &self.text
     }
 
-    pub fn get_isok(&self) -> bool{
-        self.isok
+    pub fn get_published(&self) -> bool{
+        self.published
     }
 
-    pub async fn create(pool: &SqlitePool, new_poll: NewTip)
-            -> Result<Option, sqlx::Error>{
+    pub async fn create(pool: &SqlitePool, new_tip: NewTip)
+            -> Result<Tip, sqlx::Error>{
         tracing::info!("Data: {:?}", new_poll);
-        let sql = "INSERT INTO options (reto_id, option, isok
+        let sql = "INSERT INTO tips (category_id, text, published
                    VALUES ($1, $2, $3) RETURNING *;";
         query(sql)
-            .bind(new_poll.reto_id)
-            .bind(new_poll.option)
-            .bind(new_poll.isok)
+            .bind(new_tip.category_id)
+            .bind(new_tip.text)
+            .bind(new_tip.published)
             .map(Self::from_row)
             .fetch_one(pool)
             .await
     }
-    pub async fn read(pool: &SqlitePool, id: i64) -> Result<Option<Option>, sqlx::Error>{
-        let sql = "SELECT * FROM options WHERE id = $1";
+    pub async fn read(pool: &SqlitePool, id: i64) -> Result<Tip<Tip>, sqlx::Error>{
+        let sql = "SELECT * FROM tips WHERE id = $1";
         query(sql)
             .bind(id)
             .map(Self::from_row)
@@ -70,36 +70,35 @@ impl Option{
             .await
     }
 
-    pub async fn read_all(pool: &SqlitePool) -> Result<Vec<Option>, sqlx::Error>{
-        let sql = "SELECT * FROM options";
+    pub async fn read_all(pool: &SqlitePool) -> Result<Vec<Tip>, sqlx::Error>{
+        let sql = "SELECT * FROM tips";
         query(sql)
             .map(Self::from_row)
             .fetch_all(pool)
             .await
     }
 
-    pub async fn update(pool: &SqlitePool, option: Option) -> Result<Option, sqlx::Error>{
-        let sql = "UPDATE options SET reto_id = $2, option = $3, isok = $4
-                    FROM options WHERE id = $1 RETURNING * ;";
+    pub async fn update(pool: &SqlitePool, tip: Tip) -> Result<Tip, sqlx::Error>{
+        let sql = "UPDATE tips SET category_id = $2, text = $3, published = $4
+                    FROM tips WHERE id = $1 RETURNING * ;";
         query(sql)
-            .bind(option.id)
-            .bind(option.reto_id)
-            .bind(option.option)
-            .bind(option.isok)
+            .bind(tip.id)
+            .bind(tip.category_id)
+            .bind(tip.text)
+            .bind(tip.published)
             .map(Self::from_row)
             .fetch_one(pool)
             .await
     }
 
-    pub async fn delete(pool: &SqlitePool, id: i64) -> Result<Option, sqlx::Error>{
-        let sql = "DELETE from options WHERE id = $1 RETURNING * ;";
+    pub async fn delete(pool: &SqlitePool, id: i64) -> Result<Tip, sqlx::Error>{
+        let sql = "DELETE from tips WHERE id = $1 RETURNING * ;";
         query(sql)
             .bind(id)
             .map(Self::from_row)
             .fetch_one(pool)
             .await
     }
-
 }
 
 
